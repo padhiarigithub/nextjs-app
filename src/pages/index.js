5245335adccad078c695eede461db8b8fdd09ae9
padhiarigithub <111277991+padhiarigithub@users.noreply.github.com>
2023-05-02 17:08:34 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello, I am JAGAN.welcome to next js world . be here!</h1>
+      <h1>Hello, I am JAGAN.welcome to next js world .</h1>
     </div>
   )
 }
